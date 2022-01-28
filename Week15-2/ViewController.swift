db3f86fe76edd5103a6df0989e2cc937d2065c48
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-01-28 10:05:17 +0300

    add subviews

--- Week15-2/ViewController.swift
+++ Week15-2/ViewController.swift
@@ -53,3 +53,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
+// MARK: - SubViews
+extension ViewController {
+    
+    private func addSubViews() {
+        view.addSubview(numbersStackView)
+        numbersStackView.addArrangedSubview(firstTextField)
+        numbersStackView.addArrangedSubview(secondTextField)
+        numbersStackView.edgesToSuperview(excluding: .bottom, insets: .init(top: 100, left: 100, bottom: 0, right: 100), usingSafeArea: true)
+        
+        view.addSubview(multiplyButton)
+        multiplyButton.topToBottom(of: numbersStackView, offset: 100)
+        multiplyButton.leftToSuperview().constant = 50
+        multiplyButton.height((view.frame.width - (50 * 3)) / 2)
+        multiplyButton.aspectRatio(1)
+        
+        view.addSubview(divideButton)
+        divideButton.topToBottom(of: numbersStackView, offset: 100)
+        divideButton.leadingToTrailing(of: multiplyButton, offset: 50)
+        divideButton.size(to: multiplyButton)
+        
+        view.addSubview(addButton)
+        addButton.topToBottom(of: multiplyButton, offset: 50)
+        addButton.leading(to: multiplyButton)
+        addButton.size(to: multiplyButton)
+        
+        view.addSubview(subButton)
+        subButton.topToBottom(of: divideButton, offset: 50)
+        subButton.leadingToTrailing(of: addButton, offset: 50)
+        subButton.size(to: multiplyButton)
+        
+        view.addSubview(outputLabel)
+        outputLabel.topToBottom(of: multiplyButton)
+        outputLabel.bottomToTop(of: addButton)
+        outputLabel.trailing(to: divideButton)
+        outputLabel.leading(to: multiplyButton)
+        
+    }
+}
