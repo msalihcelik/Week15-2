ed6d400a9a0b74c0330ac1392813cbdf52800813
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-01-28 10:04:07 +0300

    add ui components

--- Week15-2/ViewController.swift
+++ Week15-2/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import MobilliumBuilders
+import TinyConstraints
 
 class ViewController: UIViewController {
+    
+    private let numbersStackView = UIStackViewBuilder()
+        .distribution(.fillEqually)
+        .axis(.vertical)
+        .spacing(10)
+        .build()
+    private let firstTextField = UITextFieldBuilder()
+        .placeholder("İlk sayıyı giriniz.")
+        .textAlignment(.center)
+        .borderWidth(1)
+        .build()
+    private let secondTextField = UITextFieldBuilder()
+        .textAlignment(.center)
+        .placeholder("İkinci sayıyı giriniz.")
+        .borderWidth(1)
+        .build()
+    
+    private let multiplyButton = UIButtonBuilder()
+        .title("Çarpma")
+        .borderWidth(1)
+        .build()
+    private let divideButton = UIButtonBuilder()
+        .title("Bölme")
+        .borderWidth(1)
+        .build()
+    private let addButton = UIButtonBuilder()
+        .title("Toplama")
+        .borderWidth(1)
+        .build()
+    private let subButton = UIButtonBuilder()
+        .title("Çıkarma")
+        .borderWidth(1)
+        .build()
+    
+    private let outputLabel = UILabelBuilder()
+        .text("Sonuç")
+        .textAlignment(.center)
+        .build()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
